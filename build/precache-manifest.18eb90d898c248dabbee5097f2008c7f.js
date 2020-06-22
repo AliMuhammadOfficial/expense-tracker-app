3a6a7c03c04acf5de8c8cd1c508c05ad059b167e
Ali Muhammad <AliMuhammadOfficial>
2020-06-23 00:24:33 +0500

    removed build dir from .gitignor

---
+++ build/precache-manifest.18eb90d898c248dabbee5097f2008c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6fa7a13b78e713dc9c8204c59e0113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8273e0de1de106b9859a",
+    "url": "/static/css/main.3ba66645.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8273e0de1de106b9859a",
+    "url": "/static/js/main.ebfd189c.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
